9bc604f2b0a4316fdedf42bfce0ff46c73391bdc
Olga Efimova <ov.shaikina@gmail.com>
2026-01-20 20:10:05 +0200

    login positive test

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,45 @@
+package pages;
+
+import dto.User;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver driver) {
+        setDriver(driver);
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
+    }
+
+    @FindBy(xpath = "//input[@name='email']")
+    WebElement inputEmail;
+
+    @FindBy(xpath = "//input[@name='password']")
+    WebElement inputPassword;
+
+    @FindBy(xpath = "//button[@name='login']")
+    WebElement btnLogin;
+
+    @FindBy(xpath = "//button[@name='registration']")
+    WebElement btnRegistration;
+
+    public void typeLoginRegistrationForm(String email, String password){
+        inputEmail.sendKeys(email);
+        inputPassword.sendKeys(password);
+    }
+
+    public void typeLoginRegistrationFormWithUser(User user){
+        inputEmail.sendKeys(user.getUsername());
+        inputPassword.sendKeys(user.getPassword());
+    }
+
+    public void clickBtnLogin(){
+        btnLogin.click();
+    }
+
+    public void clickBtnRegistration(){
+        btnRegistration.click();
+    }
+}
--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -1,13 +1,29 @@
 package ui_tests;
 
+import dto.User;
 import manager.AppManager;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class LoginTests extends AppManager {
     @Test
     public void loginPositiveTest(){
-        HomePage homePage = new HomePage(getDriver());
-        homePage.clickBtnlogin();
+        new HomePage(getDriver()).clickBtnlogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationForm("elton.john@gmail.com", "$John250347$");
+        loginPage.clickBtnLogin();
+
     }
+
+    @Test
+    public void loginPositiveTestWithUser(){
+        User user = new User("elton.john@gmail.com", "$John250347$");
+        new HomePage(getDriver()).clickBtnlogin();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginRegistrationFormWithUser(user);
+        loginPage.clickBtnLogin();
+
+    }
+
 }
